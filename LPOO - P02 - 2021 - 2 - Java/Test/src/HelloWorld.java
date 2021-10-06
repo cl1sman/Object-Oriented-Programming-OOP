46c0fbc2df73f022c81f06e5ccf1d5e14ed0bfeb
Cl1sman <matheusclisman@live.com>
2021-10-05 21:46:50 -0400

    Vai la

--- LPOO - P02 - 2021 - 2 - Java/Test/src/HelloWorld.java
+++ LPOO - P02 - 2021 - 2 - Java/Test/src/HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int matrizA[][] = {{4, 3},
+                            {2, 1}};
+        int matrizB[][] = {{3, 2},
+                            {4, 0}};
+        int i, j;
+
+        for(i=0; i<matrizA.length; ++i){
+            System.out.println(matrizA[i]);
+            for(j=0; j< matrizB.length; j++){
+                System.out.println((matrizB[j]));
+
+            }
+        }
     }
 }
