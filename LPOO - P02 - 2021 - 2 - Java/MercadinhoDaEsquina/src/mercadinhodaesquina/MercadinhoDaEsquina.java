1adf89ab714de52d26a6795c45f14f2bd0c772cf
Cl1sman <matheusclisman@live.com>
2021-10-11 14:20:00 -0400

    criação do projeto

---
+++ LPOO - P02 - 2021 - 2 - Java/MercadinhoDaEsquina/src/mercadinhodaesquina/MercadinhoDaEsquina.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mercadinhodaesquina;
+
+/**
+ *
+ * @author Cl1sman ¯_(ツ)_/¯
+ */
+public class MercadinhoDaEsquina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+// Fazer uma modularização, dividindo em varios modulos e funções, chamo
+// todo mundo
